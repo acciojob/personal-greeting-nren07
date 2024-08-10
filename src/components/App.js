8c6d39a648ffae70caa414d1699090b6a3600b41
NARENDRA SINGH <117838092+nren07@users.noreply.github.com>
2024-08-10 09:57:32 +0000

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [nameStr,setNameStr]=useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+        <lable for="nameField">Enter Your Name:</lable>
+        <input onChange={(e)=>{setNameStr(e.target.value)}
+          } type="text" id="nameField"></input>
+        <div>Hello {nameStr} !</div>
     </div>
   )
 }
